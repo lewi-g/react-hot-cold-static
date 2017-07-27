669a5f86aa55e7dcfafde39edb88cf122d752def
52lions06 <mckelvey.william@yahoo.com>
2017-07-27 15:07:05 -0400

    added onChange and first setState

--- src/components/game.js
+++ src/components/game.js
@@ -4,6 +4,7 @@ import Header from './header';
 import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
+import InfoModal from './info-modal';
 
 export default class Game extends React.Component {
     constructor(props) {
@@ -12,21 +13,39 @@ export default class Game extends React.Component {
             feedback: 'Make your guess!',
 			attempts: 3,
 			guesses: [10,15,25],
+            question: false
 
 
 
         }
     }
+        onWhat (question) {
+      this.setState({
+          question: true
+      });
+  }
 		onChange(e) {
         console.log(e.target.value)
 		}
 
+        onClick(e) {
+
+            
+            return <InfoModal />
+    
+        }
 		// feedbackMethod: function that changes this.state.feedback
 
 	render() {
+        if (!this.state.question) {
+            console.log('we are almost there');
+            return <InfoModal/>
+                } 
+        else if (this.state.question) {
+        console.log('else if is here!')
     return (
         <div>
-            <Header />
+            <Header onClick={() => this.onWhat(true)}/>
             <GuessSection onChange={this.onChange} feedback={this.state.feedback}/>
             <GuessCount attempts={this.state.attempts} />
             <GuessList guesses={this.state.guesses} />
